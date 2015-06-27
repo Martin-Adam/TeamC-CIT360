7cd5d797abc3d59bc432bf92ce4a629848851d8a
pratt-jeremy <pratt.jeremy1@gmail.com>
2015-06-27 16:41:43 -0600

    Here is my socket example

---
+++ portfolio/JeremySocket/src/TestSocket/DateClient.java
@@ -0,0 +1,31 @@
+package TestSocket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Trivial client for the date server.
+ */
+public class DateClient {
+
+    /**
+     * Runs the client as an application.  First it displays a dialog
+     * box asking for the IP address or hostname of a host running
+     * the date server, then connects to it and displays the date that
+     * it serves.
+     */
+    public static void main(String[] args) throws IOException {
+        String serverAddress = JOptionPane.showInputDialog(
+            "please type your IP Address.\n" + "Leave blank and press cancel if IP is unknown");
+        Socket s = new Socket(serverAddress, 9090);
+        BufferedReader input =
+            new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String answer = input.readLine();
+        JOptionPane.showMessageDialog(null, answer);
+        System.exit(0);
+    }
+}
