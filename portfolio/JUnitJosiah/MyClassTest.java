b4688004011b22083d96e5ef13c3cbb2c8169866
HendricksJosiah <HendricksJosiah@users.noreply.github.com>
2015-07-08 17:48:15 -0600

    Update MyClassTest.java
    I made the correction to multiple and I had also a few other things in typo errors that were corrected. I was able to run it on my device.

--- portfolio/JUnitJosiah/MyClassTest.java
+++ portfolio/JUnitJosiah/MyClassTest.java
@@ -8,13 +8,16 @@ import org.junit.Test;
 
 public class MyClassTest {
 
+	//This is the good example to run
 	@Test//(expected = IllegalArguemtExeption.class)
 	public void testExceptionIsThrow() {
 		MyClass tester = new MyClass();
-		tester.multiple(1000, 5);
+		tester.multiple(10, 5);
 	}
+	
+	//This is the bad example test.
 	@Test
-	public void testMutliple() {
+	public void testMutliply() {
 		MyClass tester = new MyClass();
 		assertEquals("10 x 5 must be 50", 50, tester.multiple(10, 5));
 		fail("Not yet implemented");
