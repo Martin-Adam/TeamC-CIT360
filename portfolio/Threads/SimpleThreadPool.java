702076cb1911fd121da9fa0c5519534161e7688e
HendricksJosiah <HendricksJosiah@users.noreply.github.com>
2015-07-08 17:54:35 -0600

    Create SimpleThreadPool.java

---
+++ portfolio/Threads/SimpleThreadPool.java
@@ -0,0 +1,20 @@
+package doingThreads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SimpleThreadPool {
+	
+	public static void main (String[]args) {
+		ExecutorService executor = Executors.newFixedThreadPool(5);
+		for (int i = 0; i < 10; i++) {
+			Runnable worker = new Runnables("" + i);
+			executor.execute(worker);
+		}
+		executor.shutdown();
+		while (!executor.isTerminated()) {
+		}
+		System.out.println("Finished all threads");
+	}
+
+}
